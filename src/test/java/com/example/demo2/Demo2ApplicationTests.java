2f4f7e5ba0b439476cf6661b0e4fcad5893ca3fc
wlei3 <1083954992@qq.com>
2021-06-04 17:43:18 +0800

    dev branch commit

--- src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -5,4 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Demo2ApplicationTests {
 
+    public static void main(String[] args) {
+        // commit dev branch
+    }
 }
