0c6b274b7bd062c8de32fc8e11528ae8e3635199
wlei3 <1083954992@qq.com>
2021-11-18 15:22:12 +0800

    jvm ReentrantLock

--- src/main/java/com/example/demo2/service/Demo1EntityRecordService.java
+++ src/main/java/com/example/demo2/service/Demo1EntityRecordService.java
@@ -1,25 +1,59 @@
 package com.example.demo2.service;
 
+import cn.hutool.core.util.ObjectUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.demo2.dao.Demo1EntityRecordMapper;
 import com.example.demo2.entity.Demo1Entity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wlei3
+ */
 @Slf4j
 @Service
 public class Demo1EntityRecordService extends ServiceImpl<Demo1EntityRecordMapper, Demo1Entity> {
 
-    public void order() {
-        Demo1Entity demo1Entity = baseMapper.selectById(3);
-        demo1Entity.setName("2");
-        this.saveOrder(demo1Entity);
+    /**
+     * 重复插入锁
+     */
+    private static final Lock lock = new ReentrantLock();
+
+    public Demo1Entity selectByName(String name) {
+        return getOne(Wrappers.<Demo1Entity>lambdaQuery().eq(Demo1Entity::getName, name));
     }
 
-    @Transactional
-    public void saveOrder(Demo1Entity demo1Entity) {
-        baseMapper.updateById(demo1Entity);
-        throw new NullPointerException();
+    public void insert(String name) {
+        // 模拟耗时操作
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            log.warn("Interrupted!", e);
+            Thread.currentThread().interrupt();
+        }
+
+        Demo1Entity entity = new Demo1Entity();
+        entity.setName(name);
+        save(entity);
+    }
+
+    public void selectAndInsert(String name) {
+        lock.lock();
+        try {
+            Demo1Entity demo1Entity = selectByName(name);
+            if (ObjectUtil.isNotEmpty(demo1Entity)) {
+                return;
+            }
+            insert(name);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 }
---
+++ src/test/java/com/example/demo2/service/ConcurrencyTest.java
@@ -0,0 +1,41 @@
+package com.example.demo2.service;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.*;
+
+/**
+ * 并发测试
+ *
+ * @author wlei3
+ * @since 2021/11/5 11:54
+ */
+@SpringBootTest
+class ConcurrencyTest {
+
+    @Autowired
+    private Demo1EntityRecordService recordService;
+
+    @Test
+    void concurrencyTest() throws InterruptedException {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d")
+            .build();
+
+        //Common Thread Pool
+        ExecutorService pool = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>(1024), namedThreadFactory,
+            new ThreadPoolExecutor.AbortPolicy());
+
+        // excute
+        for (int i = 0; i < 5; i++) {
+            pool.execute(() -> recordService.selectAndInsert("test"));
+        }
+
+        //gracefully shutdown (优雅关闭)
+        pool.shutdown();
+        pool.awaitTermination(60, TimeUnit.SECONDS);
+    }
+}
