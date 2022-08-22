fe66f3118913d75ca2d938772d3b156ab7cf1709
wlei3 <1083954992@qq.com>
2022-08-22 21:56:32 +0800

    如何验证线程和request并无绑定关系呢?

--- src/main/java/com/example/demo2/controller/WebUserController.java
+++ src/main/java/com/example/demo2/controller/WebUserController.java
@@ -2,10 +2,12 @@ package com.example.demo2.controller;
 
 import com.example.demo2.dto.WebUser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
@@ -45,4 +47,34 @@ public class WebUserController {
         log.info("test222222 {} {}", System.identityHashCode(WebUser.getCurrentUser()), WebUser.getCurrentUser());
         return "test1";
     }
+
+    @GetMapping("/getTest")
+    public String getTest(HttpServletRequest request) {
+        log.info("getTest {}", System.identityHashCode(request));
+        String age = request.getParameter("age");
+        log.info("age=" + age);
+        new Thread(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            String age1 = request.getParameter("age");
+            log.info("age1=" + age1);
+        }).start();
+        return "success";
+    }
+
+    @PostMapping("/test3")
+    public String test3(HttpServletRequest request) {
+        log.info("test3 {}", System.identityHashCode(request));
+        return "test3";
+    }
+
+    @PostMapping("/test4")
+    public String test4(HttpServletRequest request) {
+        log.info("test4 {}", System.identityHashCode(request));
+        return "test4";
+    }
+
 }
