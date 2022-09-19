f6ba078e2f807c320291e92d930111d484a33707
wlei3 <1083954992@qq.com>
2022-09-19 21:12:40 +0800

    只要有一个任务的执行结果不是期望的值, 立刻中断

--- src/main/java/com/example/demo2/cf/CfTest3.java
+++ src/main/java/com/example/demo2/cf/CfTest3.java
@@ -1,31 +1,64 @@
 package com.example.demo2.cf;
 
-import javax.annotation.Resource;
+import cn.hutool.core.date.StopWatch;
+
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CfTest3 {
 
-    @Resource(name = "formDimissionPool")
-    private Executors executors;
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> list = IntStream.range(0, 10).boxed().collect(Collectors.toList());
+
+        List<CompletableFuture<Integer>> cfs = list.stream()
+            .map(i -> CompletableFuture.supplyAsync(() -> {
+                return doBussness(i);
+            }).handle((integer, throwable) -> {
+                if (throwable != null) {
+                    return -1;
+                }
+                return integer;
+            })).collect(Collectors.toList());
+
+        CompletableFuture<List<Integer>> sequence1 = FutureUtils.sequence(cfs);
 
 
-    public static void main(String[] args) {
-        List<Integer> list = IntStream.range(0, 100).boxed().collect(Collectors.toList());
-        CompletableFuture.runAsync(() -> {
-
-        });
+        // 再构造一个当有一个失败则失败的的CompletableFuture
+        CompletableFuture<Integer> anyException = new CompletableFuture<>();
+        cfs.forEach(cf -> cf.handle((r, e) -> {
+            if (r == 3)
+                return anyException.complete(r);
+            return r;
+        }));
+        // 让allComplete和anyException其中有一个完成则完成
+        // 如果allComplete有一个异常，anyException会成功完成，则整个就提前完成了
+        StopWatch sw2 = new StopWatch();
+        sw2.start();
+        CompletableFuture<Object> sequence2 = CompletableFuture.anyOf(sequence1,
+            anyException);
+        Object join = sequence2.join();
+        System.out.println(join);
+//        System.out.println(sequence1.join());
+        sw2.stop();
+        System.out.println("sw2=======>"+sw2.getTotalTimeMillis());
     }
 
-    public static void doBussness(Integer ii) {
-        // sleep(1000);
+    public static Integer doBussness(Integer ii) {
+
         try {
-            TimeUnit.SECONDS.sleep(2);
+            if (ii == 5) {
+                TimeUnit.SECONDS.sleep(4);
+            } else if (ii == 3) {
+                throw new NullPointerException();
+            } else {
+                TimeUnit.SECONDS.sleep(1);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(ii);
+        return ii;
     }
 }
