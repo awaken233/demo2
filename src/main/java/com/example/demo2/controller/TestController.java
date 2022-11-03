5ac576bd269f566489f6d327ffbf252b27184a5d
wlei3 <1083954992@qq.com>
2022-11-03 21:53:30 +0800

    ValueOperations

--- src/main/java/com/example/demo2/controller/TestController.java
+++ src/main/java/com/example/demo2/controller/TestController.java
@@ -5,9 +5,11 @@ import com.example.demo2.service.HrWorkUnitService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.Map;
 
@@ -22,13 +24,16 @@ public class TestController {
     @Autowired
     private HrWorkUnitService hrWorkUnitService;
 
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, String> valueOperations;
+
     @Autowired
     private One one;
 
     @PostMapping("/test")
     @SneakyThrows
     public Map<String, Object> test() {
-        one.test();
+        valueOperations.set("test", "asdf1232");
         return Collections.emptyMap();
     }
 }
