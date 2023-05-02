c1c1d205904bd2e6a9336b26ba0056a19480ad3e
wlei3 <1083954992@qq.com>
2023-05-02 23:41:36 +0800

    kafka

---
+++ src/main/java/com/example/demo2/Demo01Message.java
@@ -0,0 +1,21 @@
+package com.example.demo2;// Demo01Message.java
+
+public class Demo01Message {
+
+    public static final String TOPIC = "DEMO_01";
+
+    /**
+     * 编号
+     */
+    private Integer id;
+
+    // ... 省略 set/get/toString 方法
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
