a948a0190b6f9d02ac057540513e2e4e81154d9d
evtok <wangl@digilink.link>
2024-11-08 10:11:50 +0800

    test

--- src/main/java/com/example/demo2/controller/TestController.java
+++ src/main/java/com/example/demo2/controller/TestController.java
@@ -29,7 +29,7 @@ public class TestController {
     @PostMapping("/test")
     @SneakyThrows
     public void test() {
-        String[] codes = {"0d1IRPFa1vdFtI0KBEGa1ZrbZk1IRPFr", "0a1lLC100SjS7T1vCo4006Q5bv1lLC1L", "0c1sUr2001b37T12jP1005S5dU1sUr2s", "0b1fzw0001wY8T19NY0004QIzh1fzw0P", "0a1ewf0004zf9T1Eyu100rgLoj0ewf0n", "0b1XEy1w3rCbN33gIb4w3UPVwL2XEy1v", "0f1ORPFa1ydFtI0G1IJa11yTD40ORPFx", "0f1gwf0007zf9T1nkm000J0j5B4gwf08", "0f1nF41008Cw9T10OX100CzGbF3nF41B", "0b1iwf0009zf9T17Dl200p1Xsi3iwf07"};
+        String[] codes = {"0c1Xay000btM9T1ZRa4005OQ0n3Xay0d", "0f11Mall2U3pue4Jcyml2TIQ2801Mal7", "0e14Prll2R08ue4RZrnl2ydP1x04Prli"};
         for (String code : codes) {
             executor.execute(() -> test1(code));
         }
@@ -42,6 +42,7 @@ public class TestController {
         Map resp = testFeignClent.test1(param);
         if ((Integer) resp.get("code") != 0) {
             log.info("loginV3 error {}", resp);
+            return resp;
         }
         log.info("loginV3 success {}", resp);
         return resp;
